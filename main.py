79fd6ece7f06c6871d06d3ce6d1e082f637b0b2d
Arthur Oliveira de Melo <aomelo@Live.com>
2021-12-08 21:28:00 -0300

    migrate from youtube-dl to yt-dlp

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 import console
+import dialogs
+import clipboard
 import sys
 import glob
 import os
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
+from objc_util import on_main_thread
 
-if len(sys.argv) != 2:
-	raise IndexError('usage: %s url' % (sys.argv[0]))
-url = sys.argv[1]
+#Screen always on.
+on_main_thread(console.set_idle_timer_disabled)(True)
+
+url = clipboard.get()
 print('url: %s' % (url))
 
-choices = (
-	('Audio', 'bestaudio[ext=m4a]', 'm4a'),
-	('Video', 'best[ext=mp4]', 'mp4'),
-)
-choice = console.alert('youtube-dl', 'Version to extract:',
-	*(c[0] for c in choices))
+#List formats
+with YoutubeDL() as ydl:
+	meta = ydl.extract_info(url, download=False) 
+	formats = meta.get('formats', [meta])
+	
+formats_string = ['bestaudio[ext=m4a] AudioOnly m4a', 'best[ext=mp4] BestVideo mp4'] + [item['format_id']+' '+item['format_note']+' '+item['ext']+' '+item['vcodec'] for item in formats]
 
-_, format, ext = choices[choice-1]
-print('format: %s' % (format))
-
+#Select AVC format to preview on iOS.
+format_choise = dialogs.list_dialog(title = 'Formats', items = formats_string)
+	
 opts = {
-	'format': format
+	'format': format_choise.split()[0]
 }
+
 with YoutubeDL(opts) as ydl:
 	ydl.download([url])
 
-file = max(glob.glob('*.'+ext), key=os.path.getctime)
+#Share file
+file = max(glob.glob('*.'+format_choise.split()[2]), key=os.path.getctime)
 if not file:
 	raise IndexError('downloaded file not found')
 print('downloaded: %s' % (file))
 
 try:
-	console.open_in(file)	
+	if format_choise.split()[2] == 'webm':
+		console.open_in(file)
+	else:
+		console.quicklook(file)
 finally:
 	os.remove(file)
 	print('deleted: %s' % (file))
